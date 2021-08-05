0273e01b81c78359a8c3955b5a69825d61e9250d
Elvis Aguilar <elat122e@gmail.com>
2021-08-04 21:24:59 -0600

    funcion que verifica el primer caracter del token

--- src/main/java/com/practica1/proceso/Proceso.java
+++ src/main/java/com/practica1/proceso/Proceso.java
@@ -1,6 +1,7 @@
 
 package com.practica1.proceso;
 
+import com.practica1.enums.Identificador;
 import javax.swing.JTextArea;
 
 /**
@@ -12,11 +13,43 @@ public class Proceso {
     private JTextArea cadena;
     private char caracter;
     private String token;
+    private int temporal = 0;
+
+    public Proceso(JTextArea cadena) {
+        this.cadena = cadena;
+    }
+    public void Identificar(JTextArea areaLista){
+        
+    }
+    public String token(){
+        String token = "" ;
+        for (int i = temporal; i < cadena.getText().length(); i++) {
+            if (Character.compare(cadena.getText().charAt(i),(Identificador.ESPACIO.getForma().charAt(0))) == 0) {
+                this.temporal++;
+                break;
+            }else{
+                token = token+cadena.getText().charAt(i);
+                this.temporal++;
+            }
+        }
+        return token;
+    }
     
     
+    public boolean esIdentificador(String token){
+        if (Character.isLetter(token.charAt(0))) {
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
     
-    
-    
-    
-    
+    public void verificarEsNumero(String token){
+        for (int i = 0; i < token.length(); i++) {
+            if (Character.isDigit(token.charAt(i))) {
+                
+            }
+        }
+    }  
 }
